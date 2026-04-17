c6d13c8ff8e0930d8e152c1fd69cda62bf21c019
Asharinlexa-source <Asharin_lexa@mail.ru>
2026-04-17 22:43:50 +0300

    Update run.sh

--- vosk_tts/run.sh
+++ vosk_tts/run.sh
@@ -4,7 +4,7 @@ MODEL_PATH="/data/vosk-model-tts-ru-0.10-multi"
 
 echo "Starting Vosk TTS..."
 
-python3 -m wyoming_vosk3 \
+python3 -m wyoming_vosk \
   --uri tcp://0.0.0.0:10200 \
   --vosk-model-path "${MODEL_PATH}" \
   --speech-rate 1.0 \
