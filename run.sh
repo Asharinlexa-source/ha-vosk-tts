a4c4f909dd5fe30ef54fa64a6798d6cd101200b8
Asharinlexa-source <Asharin_lexa@mail.ru>
2026-04-17 21:43:18 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+MODEL_PATH="/data/vosk-model-tts-ru-0.10-multi"
+
+echo "Starting Vosk TTS..."
+
+python3 -m wyoming_vosk3 \
+  --uri tcp://0.0.0.0:10200 \
+  --vosk-model-path "${MODEL_PATH}" \
+  --speech-rate 1.0 \
+  --streaming
